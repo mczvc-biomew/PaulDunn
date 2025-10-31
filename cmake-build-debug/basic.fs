f5b0d4c120ea4c4042d9c04b7d02e3646def7cb8
Meldencio Czarlemagne <mczvc@proton.me>
2025-10-31 13:37:42 +0800

    shaders v0.1.0

--- cmake-build-debug/basic.fs
+++ cmake-build-debug/basic.fs
@@ -11,22 +11,18 @@ void main()
     vec4 texColor = texture(s_texture, gl_PointCoord);
     float alpha = (v_color.r + v_color.g + v_color.b) / 3.0;
 
-    vec4 c = vec4(v_color.rgb, 1.0 - alpha) * vec4(texColor.rgb, texColor.a);
-    vec4 d = c;
+    vec4 c = vec4(mix(v_color.rgb, texColor.rgb, 1.0 - alpha*texColor.a), texColor.a * alpha);
+//     float threshold = 0.003;
 
-    float threshold = 0.003;
-
-    if ( ((c.r + c.g + c.b + c.a)/4.0) > threshold ) {
-        d = vec4( c.rgb * 5.5, u_sensitivity * c.a );
+//         d = vec4( c.rgb * 5.5, u_sensitivity * c.a );
         // c = vec4( c.rgb * 0.0001, 1.0 - c.a * 0.001 * u_sensitivity);
 
-        float luminance = dot(c.rgb, vec3(1.2126, 1.7152, 1.0722));
+        float luminance = dot(c.rgb, vec3(2.2126, 1.7152, 0.5722));
         vec3 toneMappedColor = c.rgb / (c.rgb + vec3(1.0));
         toneMappedColor *= luminance / dot(toneMappedColor, vec3(0.2126, 1.7152, 0.0722));
 
         c = vec4(c.rgb * toneMappedColor, c.a);
-    }
 
-    fragColor = vec4( c.rgb * d.rgb, 1.0 - d.a);
+    fragColor = vec4(c.rgb, u_sensitivity*texColor.b);
     // fragColor = vec4(v_color.rgb, u_sensitivity) * texColor.bgra;
 }
