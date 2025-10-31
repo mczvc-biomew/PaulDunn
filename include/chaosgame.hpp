604eea7c411ca90799a346917fe2f6d26bcbf4a3
Meldencio Czarlemagne <mczvc@proton.me>
2025-10-31 14:57:31 +0800

    [ChaosGame]

---
+++ include/chaosgame.hpp
@@ -0,0 +1,23 @@
+#ifndef CHAOSGAME_HPP
+/** @file chaosgame.hpp
+ *
+ */
+#define CHAOSGAME_HPP
+
+#include <cmath>
+#include <vector>
+#include <chrono>
+#include <cstdio>
+
+#include <SDL2/SDL.h>
+
+#include "egg2d.h"
+
+
+void InitCGame();
+
+void RenderCGame();
+
+void ShutdownCGame();
+
+#endif
